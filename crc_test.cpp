aa05bb19c35f051b999f8ab92a78795ca9d22abf
KoynovStas <skojnov@yandex.ru>
2015-11-24 14:18:15 +0300

    add test to calculate the CRC-16/DECT-X

--- crc_test.cpp
+++ crc_test.cpp
@@ -1000,6 +1000,26 @@ int test_crc16_7(struct test_info_t  *test_info)
 
 
 
+//width=16  poly=0x0589  init=0x0000  refin=false  refout=false  xorout=0x0000  check=0x007f  name="CRC-16/DECT-X"
+int test_crc16_8(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+    uint16_t crc;
+
+    CRC_t ucrc(16, 0x0589, 0x0, false, false, 0x0);
+
+    crc = ucrc.get_crc("123456789", 9);
+    if( crc != 0x007f )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
 ptest_func tests[] =
 {
 
@@ -1070,6 +1090,7 @@ ptest_func tests[] =
     test_crc16_5,
     test_crc16_6,
     test_crc16_7,
+    test_crc16_8,
 
 };
 
