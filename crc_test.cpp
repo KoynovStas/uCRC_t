04ab1cb1fc2f8fd21c307d9114442eddfc1e26a0
KoynovStas <skojnov@yandex.ru>
2015-11-24 13:21:05 +0300

    add tests for name field

--- crc_test.cpp
+++ crc_test.cpp
@@ -4,8 +4,53 @@
 
 
 
+
+//------------- tests for CRC_t methods -------------
+
+
+int test_crc_t_name(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+
+    CRC_t  crc;
+
+
+    if( crc.name != "CRC-32" )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
+int test_crc_t_name_2(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+    const char* name = "some_name";
+
+    CRC_t crc(name);
+
+
+    if( crc.name != name )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
 ptest_func tests[] =
 {
+
+    //CRC_t methods
+    test_crc_t_name,
+    test_crc_t_name_2,
 
 };
 
