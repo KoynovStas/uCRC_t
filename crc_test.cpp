d219b1df357be165b1f3f84fc29ba75a74f0c3eb
KoynovStas <skojnov@yandex.ru>
2015-11-24 13:30:12 +0300

    add test for methods get_ for constructor params

--- crc_test.cpp
+++ crc_test.cpp
@@ -45,12 +45,132 @@ int test_crc_t_name_2(struct test_info_t  *test_info)
 
 
 
+int test_crc_t_get_bits(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+
+    CRC_t crc(1, 0, 0, true, true, 0);
+
+
+    if( crc.get_bits() != 1 )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
+int test_crc_t_get_poly(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+
+    CRC_t crc(1, 123, 0, true, true, 0);
+
+
+    if( crc.get_poly() != 123 )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
+int test_crc_t_get_init(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+
+    CRC_t crc(1, 0, 1234, true, true, 0);
+
+
+    if( crc.get_init() != 1234 )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
+
+int test_crc_t_get_xor_out(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+
+    CRC_t crc(1, 0, 0, true, true, 1000);
+
+
+    if( crc.get_xor_out() != 1000 )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
+
+int test_crc_t_get_ref_in(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+
+    CRC_t crc(1, 0, 0, true, true, 0);
+
+
+    if( crc.get_ref_in() != true )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
+
+int test_crc_t_get_ref_out(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+
+    CRC_t crc(1, 0, 0, true, true, 0);
+
+
+    if( crc.get_ref_out() != true )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
 ptest_func tests[] =
 {
 
     //CRC_t methods
     test_crc_t_name,
     test_crc_t_name_2,
+
+    test_crc_t_get_bits,
+    test_crc_t_get_poly,
+    test_crc_t_get_init,
+    test_crc_t_get_xor_out,
+    test_crc_t_get_ref_in,
+    test_crc_t_get_ref_out,
+
+
 
 };
 
