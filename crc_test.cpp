711efa4e893a1feedb4bfa7c3475d06d25a794d0
KoynovStas <skojnov@yandex.ru>
2015-11-24 14:15:40 +0300

    add test to calculate the CRC-16/AUG-CCITT

--- crc_test.cpp
+++ crc_test.cpp
@@ -880,6 +880,26 @@ int test_crc16(struct test_info_t  *test_info)
 
 
 
+//width=16  poly=0x1021  init=0x1d0f  refin=false  refout=false  xorout=0x0000  check=0xe5cc  name="CRC-16/AUG-CCITT"
+int test_crc16_2(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+    uint16_t crc;
+
+    CRC_t ucrc(16, 0x1021, 0x1d0f, false, false, 0x0);
+
+    crc = ucrc.get_crc("123456789", 9);
+    if( crc != 0xe5cc )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
 ptest_func tests[] =
 {
 
@@ -944,6 +964,7 @@ ptest_func tests[] =
     test_crc15_2,
 
     test_crc16,
+    test_crc16_2,
 
 };
 
