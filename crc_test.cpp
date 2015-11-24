0e2d1c08c6f5971ca4d6b64aaf42baa28caaed57
KoynovStas <skojnov@yandex.ru>
2015-11-24 14:20:21 +0300

    add test to calculate the CRC-16/MCRF4XX

--- crc_test.cpp
+++ crc_test.cpp
@@ -1100,6 +1100,26 @@ int test_crc16_12(struct test_info_t  *test_info)
 
 
 
+//width=16  poly=0x1021  init=0xffff  refin=true  refout=true  xorout=0x0000  check=0x6f91  name="CRC-16/MCRF4XX"
+int test_crc16_13(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+    uint16_t crc;
+
+    CRC_t ucrc(16, 0x1021, 0xffff, true, true, 0x0);
+
+    crc = ucrc.get_crc("123456789", 9);
+    if( crc != 0x6f91 )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
 ptest_func tests[] =
 {
 
@@ -1175,6 +1195,7 @@ ptest_func tests[] =
     test_crc16_10,
     test_crc16_11,
     test_crc16_12,
+    test_crc16_13,
 
 };
 
