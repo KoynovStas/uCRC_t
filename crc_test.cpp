942768610dcb464b3add4b7a3014b48fa46ab040
KoynovStas <skojnov@yandex.ru>
2017-04-06 15:10:53 +0300

    Tests: add to CRC list CRC-10/GSM

--- crc_test.cpp
+++ crc_test.cpp
@@ -69,8 +69,9 @@ const CRC_Spec_Info  CRC_List[] =
     { "CRC-8/WCDMA",      8, 0x9B, 0x0,  true,  true,  0x0,  0x25 },
 
     // CRC-10
-    { "CRC-10",          10, 0x233, 0x0,   false, false, 0x0, 0x199 },
-    { "CRC-10/CDMA2000", 10, 0x3D9, 0x3FF, false, false, 0x0, 0x233 },
+    { "CRC-10",          10, 0x233, 0x0,   false, false, 0x0,   0x199 },
+    { "CRC-10/CDMA2000", 10, 0x3D9, 0x3FF, false, false, 0x0,   0x233 },
+    { "CRC-10/GSM",      10, 0x175, 0x0,   false, false, 0x3FF, 0x12A },
 
     // CRC-11
     { "CRC-11", 11, 0x385, 0x1A, false, false, 0x0, 0x5A3 },
